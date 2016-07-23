9c8d2f5caf72666fdafa66391a7504802cc56329
Manojkumar <msvmanojkumarreddy@gmail.com>
2016-07-23 09:26:29 +0530

    Developed with Django

---
+++ website/website/music/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import url
+from . import views
+app_name = 'music'
+
+
+urlpatterns = [
+    #/music/
+    url(r'^$', views.index, name = "index"),
+    #/music/albumid/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name = "detail"),
+    #/music/albumid/favourtie/
+    url(r'^(?P<album_id>[0-9]+)/favourite/$', views.favourite, name = "favourite"),
+
+
+]
